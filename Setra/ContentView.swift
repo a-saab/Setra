02bd4b36a6dc66210684bb88d4697a38da761f67
Adnan Saab <adnan.9821@gmail.com>
2026-04-06 19:06:52 -0400

    Initial Commit

---
+++ Setra/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Setra
+//
+//  Created by Adnan Saab on 2026-04-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Setra/SetraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetraApp.swift
+//  Setra
+//
+//  Created by Adnan Saab on 2026-04-06.
+//
+
+import SwiftUI
+
+@main
+struct SetraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
